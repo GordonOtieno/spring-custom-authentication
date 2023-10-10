a1b855984de67bf232311e9b0da6e97232a4e4ae
gordonotieno <otienogordon95@gmail.com>
2023-10-11 01:05:39 +0300

    Initial project setup

---
+++ src/test/java/com/gordon/client/CustomSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gordon.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
