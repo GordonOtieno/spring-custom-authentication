b613c3ad48a2c11c9141b33e181abeeb6e979983
gordonotieno <otienogordon95@gmail.com>
2023-10-11 21:24:46 +0300

    create project configuration files

---
+++ src/main/java/com/gordon/client/config/WebSecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.gordon.client.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+
+public class WebSecurityConfiguration {
+
+	private static final String[] WHITE_LIST_URLS = { "/hello", "/register", "/verifyRegistration*",
+			"/resendVerifyToken*" };
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(11);
+	}
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(
+				auth -> auth.requestMatchers(WHITE_LIST_URLS).permitAll().requestMatchers("/api/**").authenticated())
+				.httpBasic(Customizer.withDefaults()).build();
+	}
+
+}
